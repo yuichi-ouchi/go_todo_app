2943a64400a41187a0a2413a7c327d50611d7c8d
yuichi.ohuchi <yuichi.ouchi@gmail.com>
2022-11-21 19:12:35 +0900

    make http server run method.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yuichi-ouchi/go_todo_app
 
 go 1.19
+
+require golang.org/x/sync v0.1.0
--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":9090",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
-		}),
-	)
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v ", err)
-		os.Exit(1)
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server:%v", err)
 	}
+}
+
+func run(ctx context.Context, l net.Listener) error {
+	// 引数でnet.Litnerを受け取る（addr フィールドは使わない）
+	s := &http.Server{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+
+	//別ゴルーチンでHTTPサーバを起動する
+	eg.Go(func() error {
+		// ErrServerClosedは http.Server.Shutdownが正常終了したことを示す（異常ではない）
+		if err := s.Serve(l); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	//チェネルからの終了通知を待機
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	// Goメソッドで起動した別のゴルーチン終了を待つ
+	return eg.Wait()
 }
