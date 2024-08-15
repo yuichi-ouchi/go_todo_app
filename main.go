e10b5a82ffb56e4fa672c3c12741428451aac8d1
yuichi.ohuchi <yuichi.ouchi@gmail.com>
2024-08-15 23:42:25 +0900

    use new server and mux

--- main.go
+++ main.go
@@ -5,14 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/yuichi-ouchi/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -43,33 +40,7 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	// 引数でnet.Litnerを受け取る（addr フィールドは使わない）
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			//! For test command Interrupt signal.
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-
-	//別ゴルーチンでHTTPサーバを起動する
-	eg.Go(func() error {
-		// ErrServerClosedは http.Server.Shutdownが正常終了したことを示す（異常ではない）
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	//チェネルからの終了通知を待機
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-
-	// Goメソッドで起動した別のゴルーチン終了を待つ
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
