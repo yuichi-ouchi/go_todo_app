dfb29ed22a4ae230b0d0d3daf063d9d565c72b06
yuichi.ohuchi <yuichi.ouchi@gmail.com>
2024-08-16 23:43:40 +0900

    task entity and store future

---
+++ store/store.go
@@ -0,0 +1,35 @@
+package store
+
+import (
+	"errors"
+
+	"github.com/yuichi-ouchi/go_todo_app/entity"
+)
+
+var (
+	Tasks       = &TaskStore{Tasks: map[entity.TaskID]*entity.Task{}}
+	ErrNotFound = errors.New("not found")
+)
+
+type TaskStore struct {
+	// temporary store
+	LastID entity.TaskID
+	Tasks  map[entity.TaskID]*entity.Task
+}
+
+// append task store
+func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
+	ts.LastID++
+	t.ID = ts.LastID
+	ts.Tasks[t.ID] = t
+	return t.ID, nil
+}
+
+// return task list all items
+func (ts *TaskStore) All() entity.Tasks {
+	tasks := make([]*entity.Task, len(ts.Tasks))
+	for i, t := range ts.Tasks {
+		tasks[i-1] = t
+	}
+	return tasks
+}
