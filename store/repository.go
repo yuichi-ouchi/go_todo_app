f4f175192fe9c26e73d6b2db47093fd89c32c148
yuichi.ohuchi <yuichi.ouchi@gmail.com>
2024-08-18 15:11:37 +0900

    interfaces for mysql database

--- store/repository.go
+++ store/repository.go
@@ -10,6 +10,36 @@ import (
 	"github.com/yuichi-ouchi/go_todo_app/config"
 )
 
+type Beginner interface {
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
+}
+
+type Preparer interface {
+	PreparexContext(ctx context.Context, query string) (*sqlx.Stmt, error)
+}
+
+type Execer interface {
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
+}
+
+type Queryer interface {
+	Preparer
+	QueryxContext(ctx context.Context, query string, args ...any) (*sqlx.Rows, error)
+	QueryRowxContext(ctx context.Context, query string, args ...any) *sqlx.Row
+	GetContext(ctx context.Context, dest interface{}, query string, args ...any) error
+	SelectContext(ctx context.Context, dest interface{}, query string, args ...any) error
+}
+
+var (
+	// interfaces implementation check
+	_ Beginner = (*sqlx.DB)(nil)
+	_ Preparer = (*sqlx.DB)(nil)
+	_ Queryer  = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.Tx)(nil)
+)
+
 func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
 	//"sqlx connect" execute ping command.
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
@@ -28,5 +58,4 @@ func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
 
 	xdb := sqlx.NewDb(db, "mysql")
 	return xdb, func() { _ = db.Close() }, nil
-
 }
