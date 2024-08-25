8dbdf5592cec52497dcce1133656fe0a59921d9c
yuichi.ohuchi <yuichi.ouchi@gmail.com>
2024-08-25 22:30:22 +0900

    testing kvs and database

--- testutil/db.go
+++ testutil/db.go
@@ -13,13 +13,15 @@ import (
 func OpenDBForTest(t *testing.T) *sqlx.DB {
 	t.Helper()
 
+	host := "192.168.0.20"
 	port := 3306
 	if _, defined := os.LookupEnv("CI"); defined {
+		host = "127.0.0.1"
 		port = 3306
 	}
 	db, err := sql.Open(
 		"mysql",
-		fmt.Sprintf("todo:todo@tcp(127.0.0.1:%d)/todo?parseTime=true", port),
+		fmt.Sprintf("todo:todo@tcp(%s:%d)/todo?parseTime=true", host, port),
 	)
 	if err != nil {
 		t.Fatal(err)
